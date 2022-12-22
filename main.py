edccf980f873a1e938d221727eff3f398e1bb517
Pavame <112293069+Pavame@users.noreply.github.com>
2022-12-22 16:54:05 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+print("O<")   
+
+print(" ,,")
+print("(')<   ")
+print(" ``")
+
+print(",-,")
+print("('_)<  ")
+print("`-`")
+
+print("  _")
+print(" /_|")
+print("('_)<|")
+print(" \_|   ") 
+
+class Fish:
+  def __init__(self):
+    self.level1 = level1
+    self.level2 = level2
+    self.level3 = level3
+    self.level4 = level4
+
+# Level 1
+
+
